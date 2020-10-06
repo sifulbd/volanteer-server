baca1fcac51a6a8f757a7aeeb617d22901e3bd8c
Siful Islam <sifulsmm@gmail.com>
2020-10-07 00:33:22 +0600

    files added

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(cors());
 
+const port = 5000;
+
 
 const ObjectId = require('mongodb').ObjectId;
 const MongoClient = require('mongodb').MongoClient;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eobd6.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-
-
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
 client.connect(err => {
@@ -89,6 +88,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen((process.env.PORT || 5000), () => {
+app.listen((process.env.PORT || port), () => {
     console.log('app is listening..')   
 })
